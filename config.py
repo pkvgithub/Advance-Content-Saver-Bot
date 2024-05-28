434f2ed349e7ed2897e70de6f48e16b86e4ebdf6
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-05-28 12:25:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("API_ID", "24709391")
+    API_HASH = environ.get("API_HASH", "cb1ba6bba27a3c68d219e5e34419cb5e")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7048485481:AAGpzNWV-hSxkmpXhfEIpVnL2CE-ztuIWC4") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://24x7repairsericecenter:CKHPNQA8IaEiVGpt@saveee.cbx213c.mongodb.net/")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "24x7repairsericecenter")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1923238241').split()]
 
 class temp(object): 
     lock = {}
